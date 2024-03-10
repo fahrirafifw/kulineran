0ddcc00ddbf381058ecdfafe2f2ba7388de3eca3
fahrirafifw <109947330+fahrirafifw@users.noreply.github.com>
2024-03-11 06:39:24 +0700

    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2><strong>Delicious Food Menu, </strong> <br />in Your Gadget</h2>
+          <p>Ayo Segera Pilih dan Pesan Makanan Favorit Anda</p>
+          <router-link
+            to="/"
+            class="btn btn-lg btn-success d-flex align-items-center justify-content-center"
+          >
+          <b-icon-check-circle-fill size="sm" class="me-2"></b-icon-check-circle-fill>
+        
+            Pesan
+            
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/hero.png" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroTemp",
+};
+</script>
+
+<style>
+</style>
